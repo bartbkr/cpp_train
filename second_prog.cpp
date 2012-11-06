9b3b50f196045d6daea473c356f119ead47dc6dc
Bart Baker <bartbkr@gmail.com>
2012-11-05 22:16:00 -0500

    More fooling around

--- second_prog.cpp
+++ second_prog.cpp
@@ -1,6 +1,21 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
+
+int combine_string(string a, string b)
+{
+    int counter;
+    string combined;
+    for (int counter=1; counter<3; counter++) {
+        if (counter == 1) 
+            combined = a; 
+        else if (counter == 2)
+            combined = combined + b;
+    }
+    return (combined);
+}
+
+
 
 int main()
 {
@@ -10,11 +25,13 @@ int main()
     cin>> thisisanumber;
     cin.ignore();
     cout<<"You entered: " << thisisanumber << "\n";
-    cin.get();
+    //cin.get();
     int x = 100;
     if (x == 100)
     {
       cout << "x is ";
       cout << x;
     }
+    combined = combine_string("To","gether");
+    cout << combined;
 }
